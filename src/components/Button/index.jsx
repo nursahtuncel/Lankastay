b0e3be2cb1865d749f12d12bb5703b9927d8b31a
nursahtuncel <nursahtuncell@gmail.com>
2025-01-30 00:22:14 +0300

    [JA-39] - clasName için type prop'u verildi .Type için switch-case yapısı oluşturuldu

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,24 @@
 import "./style.scss";
 
-export const Button = ({ className, children, onClick }) => {
+export const Button = ({ padding, type, children }) => {
+  switch (type) {
+    case "primary":
+      type = "primary-button";
+      break;
+    case "secondary":
+      type = "secondary-button";
+      break;
+    case "tertiary":
+      type = "tertiary-button";
+      break;
+    case "fourth":
+      type = "fourth-button";
+      break;
+    default:
+      type = "default-button";
+  }
   return (
-    <button className={className} onClick={onClick}>
+    <button style={{ padding }} className={type}>
       {children}
     </button>
   );
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,12 +1,26 @@
 import { Button } from "../../components/Button";
 import "./styles.scss";
-const handleClick = () => {
-  alert("Button clicked!");
-};
+
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Button children="Search" onClick={handleClick} />
+      <div className="buttons">
+        <Button padding="20px 120px" type="primary">
+          Book Now
+        </Button>
+        <Button padding="10px 70px" type="secondary">
+          Regiser Now
+        </Button>
+        <Button padding="20px 150px" type="tertiary">
+          Go to dashboard
+        </Button>
+        <Button padding="10px 28px" type="fourth">
+          Login
+        </Button>
+        <Button padding="20px 130px" type="default">
+          Cancel
+        </Button>
+      </div>
     </div>
   );
 };
