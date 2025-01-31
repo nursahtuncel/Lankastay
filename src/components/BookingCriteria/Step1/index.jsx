0e6315764118829f802957ba5eee83b2a42de559
nursahtuncel <nursahtuncell@gmail.com>
2025-01-31 16:01:58 +0300

    [JA-28] - step1 için css kodlarının bir kısmı yazıldı  ,resim import edildi

--- src/components/BookingCriteria/Step1/index.jsx
+++ src/components/BookingCriteria/Step1/index.jsx
@@ -1,48 +1,45 @@
 import "./style.scss";
+import img5 from "../../../assets/images/blue.svg";
+
 const FirstStep = () => {
   return (
     <div className="page1">
-      <div className="header">
-        <h2 className="h2">Booking Information</h2>
-        <p>Please fill up the blank fields below</p>
+      <div className="page-header">
+        <h2 className="title">Booking Information</h2>
+        <p className="desc">Please fill up the blank fields below</p>
       </div>
       <div className="container">
         <div className="left">
           <div className="image-div">
-            <img src="pic (1).svg" alt="" />
+            <img src={img5} alt="Blue-Origin Fams" />
           </div>
-          <div className="resminaltindakitext">
-            <div>Blue Origin Fams</div>
-            <div>Galle, Sri Lanka</div>
+          <div className="description">
+            <div className="image-desc-left">Blue Origin Fams</div>
+            <div className="image-desc-right">Galle, Sri Lanka</div>
           </div>
         </div>
-        <div className="right">
-          <div className="right-container">
-            <div className="days-count-container">
-              <div>How long you will stay?</div>
-              <div className="days">
-                <button>-</button>
-                <div>2 Day</div>
 
-                <button>+</button>
-              </div>
+        <div className="right-container">
+          <div className="days-count-container">
+            <div className="question">How long you will stay?</div>
+            <div className="selectTotalDay">
+              <button className="eksi-btn">-</button>
+              <div>2 Day</div>
+
+              <button className="arti-btn">+</button>
             </div>
-            <div className="calender-container">
-              <div>
-                <img className="calender" src="ic_calendar.svg" alt="" />
-              </div>
-              <div className="date">20january - 22january</div>
-            </div>
-            <p className="price">You will pay: $400 USD</p>
-            <p className="price">per 2 Days</p>
           </div>
+          <div className="calender-container">
+            <div>
+              <img className="calender" src="ic_calendar.svg" alt="" />
+            </div>
+            <div className="date">20january - 22january</div>
+          </div>
+          <p className="price">You will pay: $400 USD</p>
+          <p className="price">per 2 Days</p>
         </div>
       </div>
     </div>
-    //   <div className="button-div">
-    //     <button>BOOk Now</button>
-    //     <button>Cancel</button>
-    //   </div>
   );
 };
 
