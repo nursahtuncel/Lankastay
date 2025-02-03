40ebfb06df4eac20bd6c81fefb790a7a686db8cf
FurkanOzpinar <frknzpnr@gmail.com>
2025-02-03 18:56:54 +0300

    [JA-7] - Increased gaps, added most picked section to homepage

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/HeroSection";
 import "./styles.scss";
+import MostPickedSection from "../../components/MostPickedSection/MostPicked"
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <Hero />
+      <MostPickedSection />
     </div>
   );
 };
