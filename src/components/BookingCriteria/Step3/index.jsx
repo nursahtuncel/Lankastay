c691501a344dc99c0998107d0e8994d034fb1544
nursahtuncel <nursahtuncell@gmail.com>
2025-02-10 14:59:11 +0300

    [JA-28] - media query eklendi.Dashboard sayfasına yönlendirme yapıldı

--- src/components/BookingCriteria/Step3/index.jsx
+++ src/components/BookingCriteria/Step3/index.jsx
@@ -1,7 +1,12 @@
 import "./style.scss";
 import PaymentImage from "../../../assets/images/payment.svg";
 import { Button } from "../../Button";
+import { useNavigate } from "react-router-dom";
 function TertiaryStep() {
+  const navigate = useNavigate();
+  const goToDashboard = () => {
+    navigate("/dashboard");
+  };
   return (
     <div className="paymentContainer">
       <h1> Yay! Payment Completed</h1>
@@ -10,9 +15,9 @@ function TertiaryStep() {
       </div>
       <p>
         Please check your email & phone Message. We have sent all the
-        Information{" "}
+        Information
       </p>
-      <Button padding="10px 40px" type="tertiary">
+      <Button onClick={goToDashboard} padding="10px 40px" type="tertiary">
         Go to dashboard
       </Button>
     </div>
