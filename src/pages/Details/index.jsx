2b459f567424768de1ffb78157d4cd8d1be22ed5
neslyhan <neslihansuarik@outlook.com>
2025-02-12 15:07:36 +0100

    [JA-19] - amenities section component oluşturuldu, details sayfasında responsive düzeltmesi yapıldı, Booking compenent içindeki butonun text'i responsive olarak ayarlandı.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import PageTitleImplementation from "../../components/PageTitleImplementation";
 import Breadcrumb from "../../components/Breadcrumb";
 import ProductPhotoGrid from "../../components/ProductPhotoGrid";
 import BookingComponent from "../../components/BookingComponent";
+import AmenitiesSection from "../../components/AmenitiesSection";
 
 import MainPhoto from "../../assets/images/DetailsPage/product-photo.png";
 import PhotoUp from "../../assets/images/DetailsPage/product-photo-up.png";
@@ -10,15 +11,15 @@ import PhotoDown from "../../assets/images/DetailsPage/product-photo-down.png";
 
 const ImageGallery = [MainPhoto, PhotoUp, PhotoDown];
 
-
 const Details = () => {
   return (
-
     <section className="details-page">
-
       <div className="details-page-header">
         <div className="details-page-pagetitleimplementation">
-          <PageTitleImplementation title="Blue Origin Fams" description="Galle, Sri Lanka" />
+          <PageTitleImplementation
+            title="Blue Origin Fams"
+            description="Galle, Sri Lanka"
+          />
         </div>
 
         <div className="details-page-breadcrumb">
@@ -33,8 +34,19 @@ const Details = () => {
       <div className="about-and-price">
         <div className="about-and-price-about">
           <h2 className="about-title">About the place</h2>
-          <p className="about-text">Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.</p>
-          <p className="about-text">Such trends saw the demise of the soul-infused techno that typified the original Detroit sound. Robert Hood has noted that he and Daniel Bell both realized something was missing from techno in the post-rave era.</p>
+          <p className="about-text">
+            Minimal techno is a minimalist subgenre of techno music. It is
+            characterized by a stripped-down aesthetic that exploits the use of
+            repetition and understated development. Minimal techno is thought to
+            have been originally developed in the early 1990s by Detroit-based
+            producers Robert Hood and Daniel Bell.
+          </p>
+          <p className="about-text">
+            Such trends saw the demise of the soul-infused techno that typified
+            the original Detroit sound. Robert Hood has noted that he and Daniel
+            Bell both realized something was missing from techno in the
+            post-rave era.
+          </p>
         </div>
 
         <div className="booking-component-price">
@@ -42,6 +54,9 @@ const Details = () => {
         </div>
       </div>
 
+      <div>
+        <AmenitiesSection />
+      </div>
     </section>
   );
 };
