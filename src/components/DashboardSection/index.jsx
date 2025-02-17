cfd6e438ec78a230a34cf748b45d5598e0b80985
Ceyda Ünsal <ceydaunsal00@gmail.com>
2025-02-17 17:45:02 +0300

    [JA-55] - Admin-Dashboard

--- src/components/DashboardSection/index.jsx
+++ src/components/DashboardSection/index.jsx
@@ -1,22 +1,13 @@
 import "./styles.scss";
 import { Flex, Layout } from "antd";
-const { Header, Sider, Content } = Layout;
-import React from "react";
+const { Header, Sider } = Layout;
 import { DownOutlined } from "@ant-design/icons";
-import { Dropdown, Space, Table, Tag } from "antd";
-const { Column, ColumnGroup } = Table;
+import { Dropdown, Space } from "antd";
 import { Link } from "react-router-dom";
-import data from "../../constants/HotelOwners";
 import lanka from "../../assets/images/Dashboard/dashboardLankaStay.png";
 import bell from "../../assets/images/Dashboard/dashboard-bell.png";
 import profile from "../../assets/images/Dashboard/dashboard-profile.png";
-
-import img1 from "../../assets/images/Dashboard/dashboard-layout-1.png";
-import img2 from "../../assets/images/Dashboard/dashboard-layout-2.png";
-import img3 from "../../assets/images/Dashboard/dashboard-layout-3.png";
-import img4 from "../../assets/images/Dashboard/dashboard-layout-4.png";
-import icon1 from "../../assets/images/Dashboard/dashboard-icon1.png";
-import icon2 from "../../assets/images/Dashboard/dashboard-icon2.png";
+import AdminDashboard from "../AdminDashboard";
 
 const items = [
   {
@@ -67,84 +58,7 @@ const DashboardSection = () => (
             </div>
           </div>
         </Header>
-        <Content className="content-content">
-          <h1 className="content-title">Admin Dashboard</h1>
-          <div className="content-header">
-            <div className="search-container">
-              <img className="search-icon" src={img1} alt="Search Icon" />
-              <input
-                className="search-input"
-                type="text"
-                name="search-documents"
-                id="search-documents"
-                placeholder="Search documents"
-              />
-            </div>
-            <div className="dashboard-actions">
-              <div className="add-owner">
-                <p className="add-owner-text">Add Owner</p>
-                <img className="add-owner-icon" src={img2} alt="Add Owner" />
-              </div>
-              <p className="sort-text">Sort by</p>
-              <img className="sort-icon" src={img3} alt="Sort Icon" />
-              <img className="filter-icon" src={img4} alt="Filter Icon" />
-            </div>
-          </div>
-          <Table dataSource={data} className="table">
-            <ColumnGroup title="List Hotel Owners" className="column-group">
-              <Column
-                title="Name"
-                key="name"
-                className="column-name"
-                render={(record) => (
-                  <div className="name-container">
-                    <div className="name">{record.name}</div>
-                    <div className="email">{record.email}</div>
-                  </div>
-                )}
-              />
-              <Column
-                title="Role"
-                dataIndex="role"
-                key="role"
-                className="column-role"
-                render={(role) => {
-                  let buttonClass = "role-button";
-
-                  if (role === "Super Admin" || role === "Owner") {
-                    buttonClass += " role-button-blue";
-                  } else if (role === "Pending") {
-                    buttonClass += " role-button-gray";
-                  }
-
-                  return <button className={buttonClass}>{role}</button>;
-                }}
-              />
-              <Column
-                title="Create Date"
-                dataIndex="date"
-                key="date"
-                className="column-date"
-                render={(date) => <div className="date">{date}</div>}
-              />
-              <Column
-                title="Action"
-                key="action"
-                className="column-action"
-                render={(_, record) => (
-                  <Space size="middle" className="action-container">
-                    <button className="action-invite">
-                      <img src={icon1} alt="img" />
-                    </button>
-                    <button className="action-delete">
-                      <img src={icon2} alt="img" />
-                    </button>
-                  </Space>
-                )}
-              />
-            </ColumnGroup>
-          </Table>
-        </Content>
+        <AdminDashboard />
       </Layout>
     </Layout>
   </Flex>
