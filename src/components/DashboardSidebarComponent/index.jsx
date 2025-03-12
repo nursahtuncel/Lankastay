761312ae73084107a85b71503db1b60db01c3ff0
nursahtuncel <nursahtuncell@gmail.com>
2025-03-12 22:53:42 +0300

    [JA-99] - sidebarda ikonlar için active sınıfı eklendi

--- src/components/DashboardSidebarComponent/index.jsx
+++ src/components/DashboardSidebarComponent/index.jsx
@@ -2,36 +2,23 @@ import "./styles.scss";
 import { NavLink } from "react-router-dom";
 
 const menuItems = [
-  {
-    path: "/dashboard/admin",
-    icon: "resim1.svg",
-    label: "Dashboard",
-  },
+  { path: "/dashboard/admin", icon: "resimm1.svg", label: "Dashboard" },
   { path: "/dashboard/objectives", icon: "resim3.svg", label: "Objectives" },
   { path: "/dashboard/booking", icon: "resim3.svg", label: "Bookings" },
-  {
-    path: "/dashboard/refunds",
-    icon: "resim4.svg",
-    label: "Refunds",
-  },
+  { path: "/dashboard/refunds", icon: "resim4.svg", label: "Refunds" },
   { path: "/dashboard/messages", icon: "resim5.svg", label: "Message" },
-  {
-    path: "/dashboard/help",
-    icon: "resim6.svg",
-    label: "Help",
-  },
-  {
-    path: "/dashboard/settings",
-    icon: "resim7.svg",
-    label: "Settings",
-  },
+  { path: "/dashboard/help", icon: "resim6.svg", label: "Help" },
+  { path: "/dashboard/settings", icon: "resim7.svg", label: "Settings" },
 ];
 
 const DashboardSidebarComponent = () => {
   return (
     <div className="sidebar">
       <div className="logo">
-        <img src="../src/assets/images/Dashboard/Logo.svg" alt="LankaStay logo" />
+        <img
+          src="../src/assets/images/Dashboard/Logo.svg"
+          alt="LankaStay logo"
+        />
       </div>
 
       <nav className="nav">
@@ -39,12 +26,18 @@ const DashboardSidebarComponent = () => {
           <NavLink
             key={index}
             to={item.path}
-            className={({ isActive }) => (isActive ? "active" : "")}>
-            <img
-              src={`../src/assets/images/sidebar/${item.icon}`}
-              alt={`${item.label} icon`}
-            />
-            <span>{item.label}</span>
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            {({ isActive }) => (
+              <>
+                <img
+                  src={`../src/assets/images/sidebar/${item.icon}`}
+                  alt={`${item.label} icon`}
+                  className={isActive ? "active-icon" : ""}
+                />
+                <span>{item.label}</span>
+              </>
+            )}
           </NavLink>
         ))}
       </nav>
@@ -53,4 +46,3 @@ const DashboardSidebarComponent = () => {
 };
 
 export default DashboardSidebarComponent;
-
