4b09522e4a5af17fd313d7b26eaeaf916f373241
nursahtuncel <nursahtuncell@gmail.com>
2025-03-27 19:02:42 +0300

    [JA-93] - acılır-kapanır sidebar

--- src/components/DashboardSection/index.jsx
+++ src/components/DashboardSection/index.jsx
@@ -1,8 +1,11 @@
 import "./styles.scss";
-import { Flex, Layout } from "antd";
+import { useState } from "react";
+import "antd/dist/reset.css";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+
+import { Flex, Layout, Button, Menu, Dropdown, Space } from "antd";
 const { Header, Sider } = Layout;
 import { DownOutlined } from "@ant-design/icons";
-import { Dropdown, Space } from "antd";
 import { Link } from "react-router-dom";
 import lanka from "../../assets/images/Dashboard/dashboardLankaStay.png";
 import bell from "../../assets/images/Dashboard/dashboard-bell.png";
@@ -12,60 +15,66 @@ import DashboardCard from "../DashboardCard";
 import DashboardSettings from "../DashboardSettings";
 
 const items = [
-  {
-    label: <Link to="/ProfileSettings">Profile Settings</Link>,
-    key: "0",
-  },
-  {
-    label: <Link to="/Settings">Settings</Link>,
-    key: "1",
-  },
-  {
-    type: "divider",
-  },
-  {
-    label: <Link to="/">Log Out</Link>,
-    key: "2",
-  },
+  { label: <Link to="/ProfileSettings">Profile Settings</Link>, key: "0" },
+  { label: <Link to="/Settings">Settings</Link>, key: "1" },
+  { type: "divider" },
+  { label: <Link to="/">Log Out</Link>, key: "2" },
 ];
 
-const DashboardSection = () => (
-  <Flex gap="middle" wrap>
-    <Layout className="dashboard-layout">
-      <Sider width="254px" className="dashboard-sider">
-        <img className="sider-img" src={lanka} alt="img" />
-        <div className="sider-space"></div>
-      </Sider>
-      <Layout>
-        <Header className="dashboard-header">
-          <div className="dashboard-container">
-            <div className="header">
-              <h1 className="greeting-title">Hello, Hotel Owner</h1>
-              <h2 className="greeting-subtitle">Have a nice day</h2>
-            </div>
-            <div className="dashboard-user-info">
-              <img className="dashboard-icon" src={bell} alt="img" />
-              <img className="dashboard-img" src={profile} alt="img" />
-              <div className="user-profile">
-                <p className="user-name">Wejaya Raaj</p>
-                <p className="user-role">Hotel Owner</p>
+const DashboardSection = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Flex gap="middle" wrap>
+      <Layout className="dashboard-layout">
+        <Sider
+          collapsible
+          width={254}
+          collapsed={collapsed}
+          onCollapse={setCollapsed}
+          className="dashboard-sider"
+        >
+          <img className="sider-img" src={lanka} alt="img" />
+          <Button
+            style={{ backgroundColor: "#f0f2f5" }}
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            className="toggle-button"
+          />
+        </Sider>
+
+        <Layout style={{ marginLeft: collapsed ? 80 : 254 }}>
+          <Header className="dashboard-header">
+            <div className="dashboard-container">
+              <div className="header">
+                <h1 className="greeting-title">Hello, Hotel Owner</h1>
+                <h2 className="greeting-subtitle">Have a nice day</h2>
               </div>
-              <Dropdown menu={{ items }}>
-                <a onClick={(e) => e.preventDefault()}>
-                  <Space>
-                    <DownOutlined />
-                  </Space>
-                </a>
-              </Dropdown>
+              <div className="dashboard-user-info">
+                <img className="dashboard-icon" src={bell} alt="img" />
+                <img className="dashboard-img" src={profile} alt="img" />
+                <div className="user-profile">
+                  <p className="user-name">Wejaya Raaj</p>
+                  <p className="user-role">Hotel Owner</p>
+                </div>
+                <Dropdown overlay={<Menu items={items} />}>
+                  <a onClick={(e) => e.preventDefault()}>
+                    <Space>
+                      <DownOutlined />
+                    </Space>
+                  </a>
+                </Dropdown>
+              </div>
             </div>
-          </div>
-        </Header>
-        <DashboardSettings />
-        <DashboardCard />
-        <AdminDashboard />
+          </Header>
+          <DashboardSettings />
+          <DashboardCard />
+          <AdminDashboard />
+        </Layout>
       </Layout>
-    </Layout>
-  </Flex>
-);
+    </Flex>
+  );
+};
 
 export default DashboardSection;
